e9729f1aea2f07745e7aecf3f78fb2933ca7427b
Mahmood <pejvakco@gmail.com>
2021-02-16 16:59:31 +0100

    Displaying Data and Handling Events1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseService } from './course.service';
 import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [AppComponent, CourseComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [CourseService],
   bootstrap: [AppComponent],
 })
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,4 +15,41 @@ export class CourseComponent implements OnInit {
   ngOnInit(): void {}
   title = 'List of course!';
   courses;
+  isActive = false;
+  colSpan = 2;
+
+  onSave($event) {
+    console.log('Save clicked!', $event);
+    $event.stopPropagation();
+  }
+
+  onKeyUp($event) {
+    if ($event.keyCode === 13) console.log('enter Pressed!');
+  }
+
+  onKeyUpFilter() {
+    console.log('enter Pressed by filter!');
+  }
+
+  getValue1($event) {
+    console.log('this is value', $event.target.value);
+  }
+
+  getValue2(email: string) {
+    console.log('this is email', email);
+  }
+
+  userName = 'Mahmood';
+
+  twoWayBinding() {
+    console.log('this is userName', this.userName);
+  }
+
+  myObj = {
+    title: 'The complete Angular course.',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.955,
+    releaseDate: new Date(2021, 3, 16),
+  };
 }
