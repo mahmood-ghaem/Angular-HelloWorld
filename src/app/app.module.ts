0bcf42b30d961c731a535344d42413015bf86cfc
Mahmood <pejvakco@gmail.com>
2021-02-18 13:07:33 +0100

    add bootstrap-panel component and use ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { SummeryPipe } from './summery.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { CustomInputComponent } from './custom-input/custom-input.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
 
 @NgModule({
-  declarations: [AppComponent, CourseComponent, SummeryPipe, FavoriteComponent, CustomInputComponent, TitleCasePipe],
+  declarations: [AppComponent, CourseComponent, SummeryPipe, FavoriteComponent, CustomInputComponent, TitleCasePipe, BootstrapPanelComponent],
   imports: [BrowserModule, FormsModule],
   providers: [CourseService],
   bootstrap: [AppComponent],
